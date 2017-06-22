3077859fb6c5a0c31e056370a29357451b1afe63
Amanda Gault <amandagault@dhcp-10-102-190-57.wireless.northwestern.private>
2017-06-22 18:35:42 -0500

    added park api call

--- javascript/teamapp.js
+++ javascript/teamapp.js
@@ -28,3 +28,17 @@ appObj.init();
 });
 
 
+// Park API
+$.ajax({
+    url: "https://data.cityofchicago.org/resource/4xwe-2j3y.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : 
+    }
+}).done(function(data) {
+  alert("Retrieved " + data.length + " records from the dataset!");
+  console.log(data);
+});
+
+
