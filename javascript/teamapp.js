9aad75d960581349f0f69d8d946805ebb17fa05b
Amanda Gault <amandagault@dhcp-10-102-190-57.wireless.northwestern.private>
2017-06-22 19:25:00 -0500

    changed api url

--- javascript/teamapp.js
+++ javascript/teamapp.js
@@ -2,26 +2,26 @@ $(document).ready(function() {
 
 // firebase code  
 
-  firebase.initializeApp(config);
-  var database = firebase.database(); 
+//   firebase.initializeApp(config);
+//   var database = firebase.database(); 
 
 
-// App object 
-	var appObj = {
+// // App object 
+// 	var appObj = {
 
-		// global variables 
-		database: firebase.database(); 
+// 		// global variables 
+// 		database: firebase.database(); 
 
-		// functions 
-		init: function {
-			console.log ('app gowri initiated')
-		}
+// 		// functions 
+// 		init: function {
+// 			console.log ('app gowri initiated')
+// 		}
 
-	}
+// 	}
 
 
 // app initiate function 
-appObj.init();
+// appObj.init();
 
 
 
@@ -30,11 +30,11 @@ appObj.init();
 
 // Park API
 $.ajax({
-    url: "https://data.cityofchicago.org/resource/4xwe-2j3y.json",
+    url: "https://data.cityofchicago.org/resource/4xwe-2j3y.json?$where=within_circle(location, 47.59, -122.33, 1000)?location_city=FOO?location_zip=60627?location_address=500 E 130TH ST",
     type: "GET",
     data: {
-      "$limit" : 5000,
-      "$$app_token" : 
+      "$limit" : 10,
+      "$$app_token" : "qCXNmkf0nMXdkvzg6CrprJ7yf"
     }
 }).done(function(data) {
   alert("Retrieved " + data.length + " records from the dataset!");
