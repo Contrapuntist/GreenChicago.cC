a26a9ae2a1c1ec791fd81e4457194f100ca38756
Amanda Gault <amandagault@dhcp-10-102-190-57.wireless.northwestern.private>
2017-06-22 19:39:43 -0500

    changes park api

--- javascript/teamapp.js
+++ javascript/teamapp.js
@@ -30,7 +30,7 @@ appObj.init();
 
 // Park API
 $.ajax({
-    url: "https://data.cityofchicago.org/resource/4xwe-2j3y.json",
+    url: "https://data.cityofchicago.org/resource/4xwe-2j3y.json?location_address=500 E 130TH ST? https://data.cityofchicago.org/resource/4xwe-2j3y.json?$where=within_circle(location, 47.59, -122.33, 1000)?location_city=FOO?location_zip=60627",
     type: "GET",
     data: {
       "$limit" : 5000,
