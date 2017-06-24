cdb2d41085444a3f145108951bd96229436badb3
Amanda Gault <amandagault@dhcp-10-102-190-57.wireless.northwestern.private>
2017-06-24 11:48:59 -0500

    fixed my teamapp.js

--- javascript/teamapp.js
+++ javascript/teamapp.js
@@ -7,40 +7,26 @@ firebase code
 
 
 // App object 
-	var appObj = {
+  var appObj = {
 
-		// global variables 
-		database: firebase.database(); 
+    // global variables 
+    database: firebase.database(); 
 
-		// functions 
-		init: function {
-			console.log ('app gowri initiated')
-		}
+    // functions 
+    init: function {
+      console.log ('app gowri initiated')
+    }
 
-	}
+  }
 
 
 // app initiate function 
-// appObj.init();
+appObj.init();
 
 
 
 });
 
-var lat = "47.59";
-var lon = "-122.33"
 
-// Park API
-$.ajax({
-    url: "https://data.cityofchicago.org/resource/4xwe-2j3y.json?$where=within_circle(location, 41.883811, -87.631749, 1000)",
-    type: "GET",
-    data: {
-      "$limit" : 5,
-      "$$app_token" : 
-    }
-}).done(function(data) {
-  alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
-});
 
 
