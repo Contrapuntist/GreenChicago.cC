308e7bda1ea437d94efdbbd6b84bcf64f3f48296
Amanda Gault <amandagault@dhcp-10-102-190-57.wireless.northwestern.private>
2017-06-24 11:20:30 -0500

    moved park api call to its own js file

---
+++ javascript/parkapi.js
@@ -0,0 +1,15 @@
+var lat = "47.59";
+var lon = "-122.33"
+
+// Park API
+$.ajax({
+    url: "https://data.cityofchicago.org/resource/4xwe-2j3y.json?$where=within_circle(location, 41.883811, -87.631749, 1000)",
+    type: "GET",
+    data: {
+      "$limit" : 5,
+      "$$app_token" : 
+    }
+}).done(function(data) {
+  alert("Retrieved " + data.length + " records from the dataset!");
+  console.log(data);
+});
