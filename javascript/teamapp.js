84259b9725b654ecf7ac6b1ede499173787f6ffd
Contrapuntist <contrapuntist@gmail.com>
2017-06-25 12:35:00 -0500

    moved greenroofs api code into function

--- javascript/teamapp.js
+++ javascript/teamapp.js
@@ -9,11 +9,12 @@ $(document).ready(function() {
 
     // global variables 
     database: firebase.database(),
+    searchInput: null,
 
     // functions 
     init: function () {
       console.log ('app gowri initiated')
-    }
+    } 
 
   };
 
@@ -21,7 +22,23 @@ $(document).ready(function() {
 // app initiate function 
 appObj.init();
 
+
+$('#searchbtn').on('click', function() { 
+	console.log('search button clicked');
+	appObj.searchInput = $("#srchinput").val().trim();
+	console.log(appObj.searchInput);
+
+
+	// *** Possible option to check for valid input. But need method to sift through firebase too.  
+    
+    // if (inpObj.checkValidity() == false) {
+    //     document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    // }
+})
+
+
 });
+
 
 
 
