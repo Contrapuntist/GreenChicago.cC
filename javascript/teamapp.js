f9e0e57892679c1046378777fdc8b876a45bfcef
Contrapuntist <contrapuntist@gmail.com>
2017-06-25 14:10:38 -0500

    edits to html with search and adding USPS ajax call not done though

--- javascript/teamapp.js
+++ javascript/teamapp.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-// firebase code  
- 
+
 
 
 // App object 
@@ -9,7 +8,7 @@ $(document).ready(function() {
 
     // global variables 
     database: firebase.database(),
-    searchInput: null,
+    address: null,
 
     // functions 
     init: function () {
@@ -22,19 +21,54 @@ $(document).ready(function() {
 // app initiate function 
 appObj.init();
 
+console.log (database.ref("greenRoofs").orderByChild(""));
 
-$('#searchbtn').on('click', function() { 
-	console.log('search button clicked');
-	appObj.searchInput = $("#srchinput").val().trim();
-	console.log(appObj.searchInput);
+	$('#searchbtn').on('click', function() { 
+		console.log('search button clicked');
+		appObj.address = $("#srchinput").val().trim();
+		console.log(appObj.address);
 
 
 	// *** Possible option to check for valid input. But need method to sift through firebase too.  
     
     // if (inpObj.checkValidity() == false) {
     //     document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    // } 
+
+    // ref.child("users").orderByChild("ID").equalTo("U1EL5623").once("value", function(snapshot) {
+    // var userData = snapshot.val();
+    // if (userData){
+    //   console.log("exists!");
     // }
+	// });
+
+
+
+	});
+
+
+// prepping USPS ajax call for address validation on search input.    
+
+var uspsurl = null;  
+
+// AddressValidateRequest USERID=uspsID 
+
+$.$.ajax({
+	url: uspsurl, 
+	type: 'default GET (Other values: POST)',
+	dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+	data: {param1: 'value1'},
 })
+.done(function() {
+	console.log("success");
+})
+.fail(function() {
+	console.log("error");
+})
+.always(function() {
+	console.log("complete");
+});
+
 
 
 });
