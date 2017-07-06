0b4ee5057f4baf9fd8f059ce51862613863f14c9
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-07-05 21:11:00 -0500

    fixed park API issue, now lat and long updated and other data retained for each location

--- javascript/parkapi.js
+++ javascript/parkapi.js
@@ -1,9 +1,8 @@
-
- // require("./gmap");
 var database = firebase.database();
 
-var parkArray = [];
+var parksData = [];
 var loopObject = {};
+var addressArray=[];
 
 // Park API
 function getParks() {
@@ -19,47 +18,63 @@ function getParks() {
 			
 			if(data[i].location_zip) {
 				var name = (data[i].park_name);
-				var latitude = " ";
-				var longitude = " ";
+				var latitude = ' ';
+				var longitude = ' ';
 				var city = "CHICAGO";
 				var zip = (data[i].location_zip);
 				var address = (data[i].location_address);
 				var state = "IL";
 				var id = (data[i].park_number);
-				var tmpAddress=[];
-				tmpAddress.push(address.split(' ').join('+'), city.split(' ').join('+'), state.split(' ').join('+'));
-	    		var curAddress=tmpAddress.join(',');
-	    		var getLatLng="https://maps.googleapis.com/maps/api/geocode/json?key="+geoCodeKey+"&address="+curAddress;
-			   $.ajax({
-			      url: getLatLng,
-			      method: "GET"
-			      }).done(function(response){
-			      	if(response.results[0].geometry){
-			      	loopObject = {
-							lat: response.results[0].geometry.location.lat,
-							long: response.results[0].geometry.location.lng,
-							zip: zip,
-							address: address,
-							city: city,
-							state: state,
-							name: name,
-							id: id
-						};
-			     	parkArray.push(loopObject); 
-			     	}  
-		        
-		    		});
 
-
-		}			
+				loopObject = {
+					lat: latitude,
+					long: longitude,
+					zip: zip,
+					address: address,
+					city: city,
+					state: state,
+					name: name,
+					id: id
+				};
+			}
+		
+			parksData.push(loopObject);
 		}
-		console.log(parkArray);		
-	  	
-	}).then(function(){	
-		database.ref("parks").remove();
-		database.ref("parks").push(parkArray);
+		if(i>=data.length-1)
+			{
+				getLatLong();
+			}  	
 	});
 }
 
+var i=0;
+
+//function to get Latitude and Longitude using google geocode api
+function getLatLong(){
+	
+   	var tmpAddress=[];    
+    tmpAddress.push(parksData[i].address.split(' ').join('+'), parksData[i].city.split(' ').join('+'), parksData[i].state.split(' ').join('+'));
+    var curAddress=tmpAddress.join(',');
+    var getLatLng="https://maps.googleapis.com/maps/api/geocode/json?address="+curAddress;
+    $.ajax({
+      url: getLatLng,
+      method: "GET"
+      }).done(function(response){
+        parksData[i].lat = response.results[0].geometry.location.lat;
+        parksData[i].long = response.results[0].geometry.location.lng;
+        i++;
+        if(i<parksData.length){
+        	
+        	getLatLong();
+        }
+        else{
+			database.ref("parks").remove();
+			database.ref("parks").push(parksData);        	
+        }
+	        
+    });
+	
+	
+  }
 
 //getParks();
